5d647ebe1af7c051175f94eee8e909e48945c0c3
Dan <dan.nelson8@gmail.com>
2023-03-04 01:03:20 +0000

    updated documentation

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional
 from cog import BasePredictor, Input
 from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
 
 CACHE_DIR = 'weights'
 SEP = "<sep>"
@@ -14,17 +15,18 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        prompt: str = Input(description=f"Prompt to send to FLAN-T5. Multiple prompts supported, split with '{SEP}'"),
+        prompt: str = Input(description=f"Prompt to send to FLAN-T5."),
+        n: int = Input(description="Number of output sequences to generate", default=1, ge=1, le=5),
         max_length: int = Input(
-            description="Maximum number of tokens to generate; a word is generally 2-3 tokens",
+            description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
             ge=1,
             default=50
         ),
         temperature: float = Input(
-            description="Adjusts randomness of outputs, 1 is random and 0 is deterministic",
+            description="Adjusts randomness of outputs, greater than 1 is random and 0 is deterministic, 0.75 is a good starting value.",
             ge=0.01,
-            le=1,
-            default=0.8,
+            le=5,
+            default=0.75,
         ),
         top_p: float = Input(
             description="When decoding text, samples from the top p percentage of most likely tokens; lower to ignore less likely tokens",
@@ -33,17 +35,17 @@ class Predictor(BasePredictor):
             default=1.0
         ),
         repetition_penalty: float = Input(
-            description="Penalty for repeated words in generated text; 1 is no penalty, values greater than 1 discourage repetition",
-            ge=1,
-            le=10,
+            description="Penalty for repeated words in generated text; 1 is no penalty, values greater than 1 discourage repetition, less than 1 encourage it.",
+            ge=0.01,
+            le=5,
             default=1
-        ),
+        )
         ) -> List[str]:
-        prompts = prompt.split(SEP)
-        inputs = self.tokenizer(prompts, return_tensors="pt", padding=True).input_ids.to(self.device)
+        input = self.tokenizer(prompt, return_tensors="pt").input_ids.to(self.device)
 
         outputs = self.model.generate(
-            inputs,
+            input,
+            num_return_sequences=n,
             max_length=max_length,
             do_sample=True,
             temperature=temperature,
