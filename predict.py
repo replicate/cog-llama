1e102741c717ab3396abb3be48402cc029cd482f
Dan <dan.nelson8@gmail.com>
2023-03-15 02:12:08 +0000

    adds output streaming

--- predict.py
+++ predict.py
@@ -1,23 +1,24 @@
 from typing import List, Optional
-from cog import BasePredictor, Input
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from cog import BasePredictor, Input, ConcatenateIterator
+from transformers import LLaMATokenizer
+from typing import Any
 import torch
 
-CACHE_DIR = 'weights'
+from subclass import YieldingLlama
 
+CACHE_DIR = "weights"
 
 class Predictor(BasePredictor):
     def setup(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = LlamaForCausalLM.from_pretrained("weights", cache_dir=CACHE_DIR, local_files_only=True)
-        self.model = self.model
+        self.model = YieldingLlama.from_pretrained("weights/llama-7b", cache_dir=CACHE_DIR, local_files_only=True)
+        self.model.eval()
         self.model.to(self.device)
         self.tokenizer = LlamaTokenizer.from_pretrained("weights", cache_dir=CACHE_DIR, local_files_only=True)
 
     def predict(
         self,
         prompt: str = Input(description=f"Prompt to send to LLaMA."),
-        n: int = Input(description="Number of output sequences to generate", default=1, ge=1, le=5),
         max_length: int = Input(
             description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
             ge=1,
@@ -40,19 +41,55 @@ class Predictor(BasePredictor):
             ge=0.01,
             le=5,
             default=1
-        )
-        ) -> List[str]:
+        )) -> ConcatenateIterator[str]:
         input = self.tokenizer(prompt, return_tensors="pt").input_ids.to(self.device)
 
-        outputs = self.model.generate(
-            input,
-            num_return_sequences=n,
-            max_length=max_length,
-            do_sample=True,
-            temperature=temperature,
-            top_p=top_p,
-            repetition_penalty=repetition_penalty
-        )
-        out = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        return out
-        
+        with torch.inference_mode():
+            first_token_yielded = False
+            prev_ids = []
+            for output in self.model.generate(
+                input,
+                max_length=max_length,
+                do_sample=True,
+                temperature=temperature,
+                top_p=top_p,
+                repetition_penalty=repetition_penalty,
+            ):
+                cur_id = output.item()
+                print(f'token_id: {cur_id}')
+
+                # in order to properly handle spaces, we need to do our own tokenizing. Fun! 
+                # we're building up a buffer of sub-word / punctuation tokens until we hit a space, and then yielding whole words + punctuation.
+                cur_token = self.tokenizer.convert_ids_to_tokens(cur_id)
+                print(f"token: {cur_token}")
+                
+                # skip initial newline, which this almost always yields. hack - newline id = 13. 
+                if not first_token_yielded and not prev_ids and cur_id == 13:
+                    continue 
+                
+                # underscore means a space, means we yield previous tokens
+                if cur_token.startswith('▁'): # this is not a standard underscore. 
+                    print(f"{cur_token} starts with '_'")
+                    # first token
+                    if not prev_ids:
+                        prev_ids = [cur_id]
+                        continue
+                    
+                    # there are tokens to yield
+                    else:
+                        token = self.tokenizer.decode(prev_ids)
+                        prev_ids = [cur_id]
+
+                        if not first_token_yielded:
+                            # no leading space for first token
+                            token = token.strip()
+                            first_token_yielded = True
+                        print(f'yielding {token}')
+                        yield token
+                else:
+                    print(f"{cur_token} not starts with '_'")
+                    prev_ids.append(cur_id)
+                    continue
+
+            token = self.tokenizer.decode(prev_ids)
+            yield token
